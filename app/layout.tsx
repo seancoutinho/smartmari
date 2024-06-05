af6264450b518eeb864162c37889f35343aac876
Sean Coutinho <SeanCoutinho@my.uopeople.edu>
2024-06-05 09:18:36 +0200

    implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Mono } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-mono",
+})
+
+export const metadata: Metadata = {
+  title: "Smart Mari",
+  description: "Smart Mari is modern solution to managing finances that allows you to track what is happening with your money via connection to your bank accounts",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexMono.variable}`}>{children}</body>
+    </html>
+  );
+}
